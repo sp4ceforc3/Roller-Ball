6b9942213846da0c83deac59fa497084e00c4169
sp4ceforc3 <85077306+sp4ceforc3@users.noreply.github.com>
2023-06-30 18:39:40 +0200

    added Camera follow script and movement fixes

---
+++ Assets/Scripts/SmoothFollowScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowScript: MonoBehaviour {
+
+// The target we are following
+[SerializeField] Transform target;
+// The distance in the x-z plane to the target
+[SerializeField] float distance = 10.0f;
+// the height we want the camera to be above the target
+[SerializeField] float height = 10.0f;
+
+    void  LateUpdate ()
+    {
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        Vector3 newPos = new Vector3(target.position.x, height, target.position.z);
+        transform.position = newPos;
+        transform.position -= Vector3.forward * distance;       
+
+        // Always look at the target
+        //transform.LookAt(target);
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -13,16 +13,19 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     public InputActions playerControls;
 
-    private bool jumpw = false;
+    private bool jump = false;
     private bool godmode = false;
     private int jumpCnt = 0;
+    private GameObject player;
 
     Vector3 moveVector;
     private void Awake()
     {
         playerControls = new InputActions();
         rb = GetComponent<Rigidbody>();
+        player = this.gameObject;
     }
+
     private void OnEnable()
     {
         playerControls.Player.Jump.performed += _ => {jump = true;};
@@ -37,14 +40,22 @@ public class playerController : MonoBehaviour
     {
         moveVector = playerControls.Player.Move.ReadValue<Vector3>();
         
-        if (godmode)
-            rb.velocity = speed * moveVector;
-        else
+        if(!godmode)
+        {
+            //Debug.Log(player.transform.position);
             rb.AddForce(moveVector.x * speed, 0f, moveVector.z * speed);
-
-        // This sends a ray from the center of the ball downwards, so that we can check if the ball is on the ground
-        if (Physics.Raycast(transform.position, Vector3.down, rayLength, groundMask))
-            jumpCnt = 0;
+            var newVelocity = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0f, rb.velocity.z), 5f);
+            newVelocity.y = rb.velocity.y;
+            rb.velocity = newVelocity;
+            
+            // This sends a ray from the center of the ball downwards, so that we can check if the ball is on the ground
+            if (Physics.Raycast(transform.position, Vector3.down, rayLength, groundMask))
+                jumpCnt = 0;
+        }else
+        {
+            moveVector.Normalize();
+            gameObject.transform.position +=  moveVector * speed * Time.deltaTime;;
+        }
     }
 
     void Start()
@@ -55,17 +66,24 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Setting velocity directly makes sure that each jump is the same, regardless of current velocity
-        if (jump && jumpCnt <= 1) {
-            jumpCnt += 1;
-            rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z); 
-            jump = false;
+        if(!godmode)
+        {
+            if (jump) {
+                if(jumpCnt < 1)
+                {
+                    jumpCnt += 1;
+                    // Setting velocity directly makes sure that each jump is the same, regardless of current velocity
+                    rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z); 
+                }                
+                jump = false;
+            }
         }  
     }
 
     private void ToogleGodMode(InputAction.CallbackContext _) { 
         godmode = !godmode;
         rb.isKinematic = !rb.isKinematic;
+        rb.useGravity = !rb.useGravity;
     }
 
     // This will draw the ray into the scene. This is only visible in the Editor and not in the final game
